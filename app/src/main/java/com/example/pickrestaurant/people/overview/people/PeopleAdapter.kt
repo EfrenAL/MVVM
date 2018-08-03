d12f4c1a80d69186aaee12d1e1e76a1d6e5a0f37
Efren <efren.alvarezlamolda@takeaway.com>
2018-08-03 13:50:23 +0200

    Load images with Glide

--- app/src/main/java/com/example/pickrestaurant/people/overview/people/PeopleAdapter.kt
+++ app/src/main/java/com/example/pickrestaurant/people/overview/people/PeopleAdapter.kt
@@ -1,21 +1,18 @@
 package com.example.pickrestaurant.people.overview.people
 
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.pickrestaurant.people.R
 import com.example.pickrestaurant.people.model.User
 import kotlinx.android.synthetic.main.people_item.view.*
-import java.io.InputStream
-import java.net.URL
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.os.AsyncTask
-import android.util.Log
-import android.widget.ImageView
+import com.bumptech.glide.load.resource.drawable.GlideDrawable
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import java.lang.Exception
 
 
 /**
@@ -28,7 +25,7 @@ class PeopleAdapter(val people: List<User>, private val context: Context, privat
     }
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        holder.bind(people[position], listener, position)
+        holder.bind(people[position], listener, position, context)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): UserViewHolder {
@@ -46,12 +43,16 @@ class UserViewHolder(var view: View) : RecyclerView.ViewHolder(view) {
     private val tvDescription = view.tv_description
     private val ivPicture = view.iv_picture
 
-    fun bind(user: User, listener: PeopleAdapter.OnItemClickListener, position: Int) {
+    fun bind(user: User, listener: PeopleAdapter.OnItemClickListener, position: Int, context: Context) {
 
         tvName?.text = user.name
         tvDescription?.text = user.description
         view.setOnClickListener({
             listener.onItemClick(user, position)
         })
+
+        Glide.with(context)
+                .load("https://s3.amazonaws.com/bquini-bucket/" + user.pictureUrl)
+                .into(ivPicture)
     }
 }
